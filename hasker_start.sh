4ce1eeeb160f4ebc38432a9b15c4590b27832a33
jbuly4 <ibuly4@gmail.com>
2023-08-08 23:05:25 +0300

    added collect static to startscript

--- hasker_start.sh
+++ hasker_start.sh
@@ -3,4 +3,5 @@ chmod 777 /hasker/wait-for-it.sh
 /hasker/wait-for-it.sh db:5432
 python /hasker/manage.py migrate
 python /hasker/manage.py loaddata /hasker/hasker_data.json
+python /hasker/manage.py collectstatic --noinput
 uwsgi --ini /hasker/config/uwsgi/uwsgi.ini
